f0453692f14e70c5d7ee6ba6627b49aaf439615d
vespirhub <vespirify@hotmail.co.uk>
2019-04-12 16:02:18 +0100

    Outliers

--- ANN.py
+++ ANN.py
@@ -46,9 +46,52 @@ for i, col in enumerate(data_iter):
     norm_data[:,i] = (norm_data[:,i] - np.mean(norm_data[:,i])) / (np.std(norm_data[:,i]) + 1e-7)
 
 # Plots
-
 sns.boxplot(x=norm_data[:,0],y=data['Status']);
 
+normals = np.array(data[data['Status'] == 'Normal'])
+abnormals = np.array(data[data['Status'] == 'Abnormal'])
 
+fig = plt.figure();
+sns.kdeplot(normals[:,5], color='red', shade=True);
+sns.kdeplot(abnormals[:,5], color='blue', shade=True);
+
+# Outliers found in the rightmost side of the plot, we can prove this speculation by calculating the outliers.
+
+# Get first, second and third quartile of feature.
+Q1_N, Q2_N, Q3_N = np.percentile(normals[:,5], [25, 50, 75])
+Q1_A, Q2_A, Q3_A = np.percentile(abnormals[:,5], [25, 50, 75])
+
+# Calculate the interquartile range.
+IQR_N, IQR_A = (Q3_N - Q1_N, Q3_A - Q1_A)
+
+# Establish inner and outer fences to gauge the type of outliers found.
+inner_N1, inner_N2 = (Q1_N - IQR_N * 1.5, Q3_N + IQR_N * 1.5)
+outer_N1, outer_N2 = (Q1_N - IQR_N * 3, Q3_N + IQR_N * 3)
+inner_A1, inner_A2 = (Q1_A - IQR_A * 1.5, Q3_A + IQR_A * 1.5)
+outer_A1, outer_A2 = (Q1_A - IQR_A * 3, Q3_A + IQR_A * 3)
+
+# Allocate empty lists for outliers
+minor_normal_outliers, major_normal_outliers, minor_abnormal_outliers, major_abnormal_outliers = [[] for i in range(4)]
+
+# If values are less than the lower_inner fence or bigger than the bigger_inner fence assign as minor outlier, lands within 1.5 * the IQR
+# If values are less than the lower_outer fence or bigger than the bigger_outer fence assign as major outlier, lands within 3 * the IQR
+for i in range(len(normals)):
+    if (normals[i,5] < inner_N1) or (normals[i,5] > inner_N2):
+        minor_normal_outliers.append(normals[i,5])
+    if (normals[i,5] < outer_N1) or (normals[i,5] > outer_N2):
+        major_normal_outliers.append(normals[i,5])
+print('Found {} minor outliers and {} major outliers in the Normal Class of {}!'.format(len(minor_normal_outliers),len(major_normal_outliers),data.columns.values[5]))
+# Normals contain 15 minor outliers, 0 major outliers.
+
+# If values are less than the lower_inner fence or bigger than the bigger_inner fence assign as minor outlier, lands within 1.5 * the IQR
+# If values are less than the lower_outer fence or bigger than the bigger_outer fence assign as major outlier, lands within 3 * the IQR
+for i in range(len(normals)):
+    if (abnormals[i,5] < inner_A1) or (abnormals[i,5] > inner_A2):
+        minor_abnormal_outliers.append(abnormals[i,5])
+    if (abnormals[i,5] < outer_A1) or (abnormals[i,5] > outer_A2):
+        major_abnormal_outliers.append(abnormals[i,5])
+
+print('Found {} minor outliers and {} major outliers in the Abnormal Class of {}!'.format(len(minor_abnormal_outliers),len(major_abnormal_outliers),data.columns.values[5]))
+# Abnormals contain 16 minor outliers, 3 major outliers.
 
 # Size of data, 996,13 |||| 12 Features |||| Labels are categorical : i.e Normal Abnormal |||| Normalise so features have similar ranges for gradient descent to converge faster + no oscillation of weights.
